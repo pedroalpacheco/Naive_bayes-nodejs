774267da8c65a85b376e8ecc809db65f393bfe2b
pedroalpacheco <pedro.pacheco.a@gmail.com>
2019-04-25 10:54:34 -0300

    Novo teste de frase maior

--- start.js
+++ start.js
@@ -9,7 +9,7 @@ by.train("Daria tudo que sei pela metade do que ignoro.",'filosofia');
 by.train("Nossas dúvidas são traidoras e nos fazem perder o que, com frequência, poderíamos ganhar, por simples medo de arriscar.",'dúvida');
 by.train("O ignorante afirma, o sábio duvida, o sensato reflete.",'dúvida');
 
-let score = by.guess('A dúvida é o princípio da sabedoria.')
+let score = by.guess('Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas acima de tudo, seja você sempre.')
 
 let resultado = by.extractWinner(score);
 
